b1c78a243c2e4ae0e476687f620b2e3f7f8c22bd
poojamalagi <poojamalagi00@gmail.com>
2026-03-24 18:24:39 +0530

    Add files via upload

---
+++ 45.py
@@ -0,0 +1,5 @@
+# Print multiplication table of 5
+for i in range(1,11):
+    total=5 
+    total*=i
+    print(f"5*{i}={total}")
---
+++ revision 1.py
@@ -0,0 +1,126 @@
+# #write name,age from user an print
+# name=input("Please enter your name= ")
+# age=int(input("Please enter your age= "))
+# print(f"Hello name= {name}, next year i will be={age+1}")
+# ..............................................................................................
+#Exercise 2
+#Take a number and check Is it even or odd?
+# a=int(input("enter the number="))
+# if(a%2==0):
+#     print(a,"is even")
+# else:
+#     print(a,"is odd")
+# ..............................................................................................................
+#Exercise 3
+# Write a program to:
+# •	take two numbers
+# •	print addition, subtraction, multiplication, division
+# a=int(input("enter the value of a="))
+# b=int(input("enter the value of b="))
+# sum=a+b
+# sub=float(a-b)
+# mul=a*b
+# div=a/b
+# print(f"sum={sum}, sub={sub}, mul={mul}, div={div}")
+# ...................................................................................................................
+# Exercise 4
+# Take a number and print:
+# •	square
+# •	cube
+# num=int(input("enter the to get square and cube value ="))
+# sqau=num**2
+# cube=num**3
+# print(f"Square of{num}= {sqau}, cube are of{num}= {cube}")
+# ......................................................................................................................
+# Exercise 5
+# Take marks from user and calculate percentage.
+#math=int(input("please enter the math marks="))
+# eng=int(input("please enter the eng marks="))
+# kad=int(input("please enter the kad marks="))
+# if math&eng&kad>=35:
+#    total=math+eng+kad
+#    per=total*100/300
+#    if per>=80:
+#     print("distinction")
+#    elif per>=60:
+#     print("first class")
+#    elif per>=35:
+#     print("second class")
+#    else:
+#      print("fail")
+# else:
+#   print("one sub fail")
+# .....................................................................................................................
+# Exercise 6
+# Take a number and check:
+# •	Positive
+# •	Negative
+# •	Zero
+# num=int(input("enter the number="))
+# if num>0:
+#     print(num,"is positive")
+# elif num<0:
+#     print(num,"is negative")
+# else:
+#     print(num, "is zero")
+# ......................................................................................................................................
+# Exercise 7
+# Take age and check:
+# •	If age ≥ 18 → Eligible to vote
+# •	Else → Not eligible
+# a=int(input("enter your age"))
+# if a>=18:
+#     print("Eligible for vote")
+# else:
+#     print("not eligible for vote")
+# ..................................................................................................................
+# Exercise 8
+# Find the largest of 3 numbers.
+# a=int(input("enter the number="))
+# b=int(input("enter the second number"))
+# c=int(input("enter the 3rd number"))
+# if a>=b and a>=c:
+#     print(a,"is the largest number")
+# elif b>=a and b>=c:
+#     print(b,"largest number")
+# else:
+#     print(c,"is the largest number")
+# ....................................................................................................................
+# Exercise 9
+# Check whether a number is divisible by 5 and 11.
+# a=int(input("enter the number"))
+# if a%5==0 and a%11==0:
+#     print(f"{a} is divisible by {5} and {11}")
+# else:
+#     print(f"{a} is not divisible by {5} and {11}")
+# ......................................................................................................................
+# Exercise 11
+# Write a program using a for loop to print:
+# 1
+# 2
+# 3
+# ...
+# 10
+# for num in range(1,11):
+#     print(num)
+# ......................................................................................................................
+# Exercise 12
+# Print only even numbers from 1 to 20
+# for num in range(2,21,2):
+#     print(num)
+# ...................................................................................................................
+# Exercise 13
+# Find the sum of numbers from 1 to 10
+# total=0
+# for i in range(1,11):
+#     total+=i
+# print("total sum=",total)
+# .....................................................................................................................
+#  Exercise 14
+# Print multiplication table of 5
+# for i in range(1,11):
+#     total=5
+#     total*=i
+#     print(f"5*{i}={total}")
+
+
